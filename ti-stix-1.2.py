9b27203d0e0b6ff588bcaaa4b128bfeb4b0da052
Kyle Maxwell <kmaxwell@verisign.com>
2018-03-12 11:50:06 -0500

    Update with previous 1.2.1 version

--- ti-stix-1.2.py
+++ ti-stix-1.2.py
@@ -1,24 +1,20 @@
 # Use Python 2.7.x for this version
 #
-# Automatically ingest iDefense Threat Indicators data feed.
-# Output to STIX 1.2
+# Automatically ingest iDefense Threat Indicators API data feed.
+# Output to STIX 1.2.1
 #
-# Requires python-stix-1.2.0.0+ and python-cybox-2.1.0.12+
+# Requires python-stix-1.2.0.1+ and python-cybox-2.1.0.13+
 
-import csv
+import datetime
 import json
 import os
 import sys
-from itertools import islice
 
 import requests
+from cybox.common import Hash
 from cybox.objects.address_object import Address
 from cybox.objects.domain_name_object import DomainName
-from cybox.objects.http_session_object import HTTPClientRequest
-from cybox.objects.http_session_object import HTTPRequestHeader
-from cybox.objects.http_session_object import HTTPRequestHeaderFields
-from cybox.objects.http_session_object import HTTPRequestResponse
-from cybox.objects.http_session_object import HTTPSession
+from cybox.objects.file_object import File
 from cybox.objects.uri_object import URI
 from mixbox.idgen import set_id_namespace
 from mixbox.namespaces import Namespace
@@ -28,230 +24,192 @@ from stix.core import STIXHeader
 from stix.core import STIXPackage
 from stix.indicator import Indicator
 from stix.ttp import TTP
+from stix.ttp import MalwareInstance
+from stix.ttp.behavior import Behavior
+
+
+def add_malware(hashVal, TTPTitle, malware_uuid):
+    malware_instance = MalwareInstance()
+    malware_instance.add_name(TTPTitle)
+    # malware_instance.add_type("Malware")
+
+    ttp = TTP(title=TTPTitle)
+    ttp.behavior = Behavior()
+    ttp.behavior.add_malware_instance(malware_instance)
+
+    file_object = File()
+    file_object.add_hash(Hash(hashVal))
+    file_object.hashes[0].simple_hash_value.condition = "Equals"
+
+    indicator = Indicator(id_="indicator-{0}".format(malware_uuid), title="File hash")
+    indicator.add_indicator_type("File Hash Watchlist")
+    indicator.add_observable(file_object)
+    indicator.add_indicated_ttp(TTP(idref=ttp.id_))
+    return(indicator)
 
 
 def main():
-
-    # Define constants
-
-    # iDefense API base URL
-    BASE_URL = 'https://intelgraph.idefense.com/rest/customer/'
-
-    # Threat Indicators URL
-    TI_URL = 'threatindicator/v0'
-
+    # define constants
+    TI_REQUEST_URL = "https://api.intelgraph.verisign.com/rest/threatindicator/v0"
     # iDefense API Key
     # To avoid hard-coding creds, I'm using environment variables
-    # Encoded base64 for obfuscation (not perfect, but better than nothing)
 
-    if os.environ.get('IDEF_TOKEN') is None:
-        print "error: please store your iDefense IntelGraph API key in the IDEF_TOKEN environment variable"
+    if os.environ.get('API_KEY') is None:
+        print "error: please store your iDefense IntelGraph API key in the API_KEY environment variable"
         sys.exit(1)
 
-    API_KEY = os.environ.get('IDEF_TOKEN')
+    API_KEY = os.environ.get('API_KEY')
+    API_SECRET = ''
 
-    fullUrl = BASE_URL + TI_URL
+    # Insert the start date from when you want to retrive the TI data from.
 
-    # XXX this should be set by the user
-    LAST_IMPORT = '2015-01-01T00:00:00.000Z'
+    timestr = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    LAST_IMPORT = timestr.strftime("%Y-%m-%dT%H:%M:%S") + ".000Z"
 
-    # Set custom headers
-    headers = {'Content-Type': 'application/json', 'auth-token': API_KEY}
+    file = open('stix-1.2.1.xml', 'w')
 
-    try:
-        payload = {'start_date': LAST_IMPORT}
+    HEADERS = {
+        "Content-Type": "application/json",
+        "auth-token": API_KEY,
+        "X-Api-Key-Proof": API_SECRET
+    }
 
-        r = requests.post(fullUrl, headers=headers, data=json.dumps(payload))
-        if r.status_code == requests.codes.ok:
-            try:
-                # Read in response as JSON
-                response = r.json()
+    print HEADERS
 
-                # Iterate the response
-                for indicatorD in response['results']:
-
-                    # Indicator value such as the value of the IP/Domain/URL
-                    indicator = indicatorD.get('key')
-
-                    # The type of the indicator (IP/Domain/URL)
-                    indicator_type = indicatorD.get('type')
-
-                    # Integer value of the severity ratings, 1 representing the least severe and 5 the most severe
-                    # 'LOW' -> 2, 'MEDIUM' -> 3, 'HIGH' -> 4
-                    severity = indicatorD.get('severity')
-
-                    # Classification of the indicator (Cyber Crime, Cyber Espionage, Hacktivism)
-                    threat_types = indicatorD.get('threat_types')
-
-                    # Classification of how the indicator is being used ('MALWARE_C2', 'MALWARE_DOWNLOAD', 'EXPLOIT')
-                    type_of_use = indicatorD.get('last_seen_as')
-
-                    # Associated host based indicators
-                    if 'files' in indicatorD:
-                        for hashD in indicatorD['files']:
-                            md5 = hashD.get('key')
-                            # Malware Family classification of the hash if available
-                            malware_family = hashD.get('malware_family')
-
-                    # Associated intelligence alert providing the added context and narrative for the indicator
-                    if 'mentioned_by' in indicatorD:
-                        for intel in indicatorD['mentioned_by']:
-                            # Classification of the type of report (Intelligence Report, Intelligence Alert)
-                            report_type = intel.get('type')
-                            # Identifier for the report
-                            report_identifier = intel.get('key')
-
-                    print indicator, indicator_type, severity, threat_types, type_of_use, md5, malware_family, report_type, report_identifier
-
-            except (ValueError, KeyError) as e:
-                print "Response couldn't be decoded :("
-
-        else:
-            print "API request couldn't be fulfilled due status code: %d" % r.status_code
-
-    except requests.exceptions.ConnectionError as e:
-        print "Check your network connection\n %s" % str(e)
-        pass
-
-    except requests.exceptions.HTTPError as e:
-        print "Bad HTTP response\n %s" % str(e)
-        pass
-
-    except Exception as e:
-        print "Uncaught exception\n %s" % str(e)
-        pass
-
-    exit(0)
-
-    # Convert string to list of lines
-    modContent = r.text.splitlines()
-
-    # Convert each line into comma separated list content
-    data = csv.reader(modContent)
-
-    # TI field description
-    # type = {IPV4, DOMAIN, URL, USER_AGENT, REGISTRY_ENTRY, MUTEX} [0]
-    # format = {STRING, REGEX} [1]
-    # value = actual indicator value [2]
-    # role = {MALWARE_C2, MALWARE_DOWNLOAD, EXPLOIT, MALWARE_INFECTION} [3]
-    # sample-md5 = MD5 hash of malware sample [4]
-    # last-observed = date/time indicator observed [5]
-    # comment = Free text comment [6]
-    # ref-id = IR# for iDefense report for add'l context [7]
-    # confidence = {LOW, MEDIUM, HIGH} [8]
-    # severity = {LOW, MEDIUM, HIGH} [9]
-    # sample-sha1 = SHA1 hash of malware sample [10]
-    # sample-sha256 = SHA256 hash of malware sample [11]
+    page = 1
+    more_data = True
+    count = 0
 
     # Set namespace
-    NAMESPACE = Namespace("http://idefense.com", "idefense")
+    NAMESPACE = Namespace("https://intelgraph.verisign.com", "idefense")
     set_id_namespace(NAMESPACE)
 
     # Create STIX Package
     stix_package = STIXPackage()
     stix_header = STIXHeader()
-    stix_header.description = "iDefense Threat Indicators Feed"
+    stix_header.description = "Verisign iDefense Threat Indicators Feed"
     stix_package.stix_header = stix_header
 
     ttps = {}
+    malware = {}
 
-    # Iterate over results, ignoring first 8 header lines
-    for i in islice(data, 8, None):
-        if i[0] == "IPV4":
-            # Identify TTP
-            if i[3] not in ttps:
-                ttps[i[3]] = TTP(title=i[3])
-                stix_package.add_ttp(ttps[i[3]])
+    try:
 
-            # Create indicator
-            indicator = Indicator(title=i[6], timestamp=i[5])
-            indicator.add_indicator_type("IP Watchlist")
+        while more_data:
+            request_payload = {"start_date": LAST_IMPORT, "page_size": 200, "page": page}
+            r = requests.post(TI_REQUEST_URL, headers=HEADERS, data=json.dumps(request_payload))
+            print r
+            response = []
+            if r.status_code == requests.codes.ok:
+                try:
+                    # Read in response as json
+                    response = r.json()
 
-            # Populate IP address
-            addr = Address(address_value=i[2], category=Address.CAT_IPV4)
-            addr.condition = "Equals"
-            indicator.add_observable(addr)
-            # Link TTP
-            indicator.add_indicated_ttp(TTP(idref=ttps[i[3]].id_))
-            # Indicate confidence score
-            indicator.confidence = Confidence(value=VocabString(i[8]))
-            # Add to package
-            stix_package.add_indicator(indicator)
-        elif i[0] == "URL":
-            # Identify TTP
-            if i[3] not in ttps:
-                ttps[i[3]] = TTP(title=i[3])
-                stix_package.add_ttp(ttps[i[3]])
+                except (ValueError, KeyError) as e:
+                    print "Response couldn't be decoded :("
+                    more_data = False
+                    continue
 
-            # Create indicator
-            indicator = Indicator(title=i[6], timestamp=i[5])
-            indicator.add_indicator_type("URL Watchlist")
+                more_data = response.get('more', 'False')
+                print "Page %d ==> %s (%s)" % (page, response['more'], response['total_size'])
+                page += 1
 
-            # Populate URL
-            url = URI()
-            url.value = i[2]
-            url.type_ = URI.TYPE_URL
-            url.value.condition = "Equals"
+                # Iterate the response
+                for indicatorD in response['results']:
+                    count += 1
+                    # Indicator value such as the value of the IP/Domain/URL
+                    indicator = indicatorD.get('key')
+                    print indicator, indicatorD.get('type')
+                    # print type(indicatorD.get('last_seen_as'))
+                    if indicatorD.get('last_seen_as') is None:
+                        last_seen_as = 'UNKNOWN'
+                    else:
+                        last_seen_as = ''.join(indicatorD.get('last_seen_as'))
+                    # print 'TTP',last_seen_as
 
-            indicator.add_observable(url)
-            # Link TTP
-            indicator.add_indicated_ttp(TTP(idref=ttps[i[3]].id_))
-            # Indicate confidence score
-            indicator.confidence = Confidence(value=VocabString(i[8]))
-            # Add to package
-            stix_package.add_indicator(indicator)
-        elif i[0] == "DOMAIN":
-            # Identify TTP
-            if i[3] not in ttps:
-                ttps[i[3]] = TTP(title=i[3])
-                stix_package.add_ttp(ttps[i[3]])
+                    # Identify TTP
+                    if last_seen_as not in ttps:
+                        ttps[last_seen_as] = TTP(title=last_seen_as)
+                        stix_package.add_ttp(ttps[last_seen_as])
+                        # print "Added TTP"
 
-            # Populate domain name
-            indicator = Indicator(title=i[6], timestamp=i[5])
-            indicator.add_indicator_type("Domain Watchlist")
+                    # Identify malware source
+                    if 'files' in indicatorD:
+                        for hashD in indicatorD['files']:
+                            md5 = hashD.get('key')
+                            # print 'Hash=',md5
+                            # Malware Family classification of the hash if available
+                            # print hashD.get('malware_family')
+                            if hashD.get('malware_family') is None:
+                                malware_family = "Unknown"
+                            else:
+                                malware_family = ''.join(hashD.get('malware_family'))
+                            if md5 not in malware:
+                                malware[md5] = add_malware(md5, malware_family, hashD.get('uuid'))
+                                # print "Added malware"
 
-            domain = DomainName()
-            domain.value = i[2]
-            domain.value.condition = "Equals"
+                    if indicatorD.get('type') == "url":
+                        # Create indicator
+                        indicator = Indicator(id_="indicator-{0}".format(indicatorD.get('uuid')), title=''.join(indicatorD.get('malware_family')), timestamp=indicatorD.get('last_seen'))
+                        indicator.add_indicator_type("URL Watchlist")
 
-            indicator.add_observable(domain)
-            # Link TTP
-            indicator.add_indicated_ttp(TTP(idref=ttps[i[3]].id_))
-            # Indicate confidence score
-            indicator.confidence = Confidence(value=VocabString(i[8]))
-            # Add to package
-            stix_package.add_indicator(indicator)
-        elif i[0] == "USER_AGENT":
-            # Identify TTP
-            if i[3] not in ttps:
-                ttps[i[3]] = TTP(title=i[3])
-                stix_package.add_ttp(ttps[i[3]])
+                        # Populate URL
+                        url = URI()
+                        url.value = indicatorD.get('key')
+                        url.type_ = URI.TYPE_URL
+                        url.value.condition = "Equals"
 
-            # Populate user-agent-string
-            indicator = Indicator(title=i[6], timestamp=i[5])
-            indicator.add_indicator_type("URL Watchlist")
+                        indicator.add_observable(url)
+                        # print "Added URL"
 
-            fields = HTTPRequestHeaderFields()
-            fields.user_agent = i[2]
-            header = HTTPRequestHeader()
-            header.parsed_header = fields
-            request = HTTPClientRequest()
-            request.http_request_header = header
-            req_res = HTTPRequestResponse()
-            req_res.http_client_request = request
-            session = HTTPSession()
-            session.http_request_response = [req_res]
+                    elif indicatorD.get('type') == "domain":
+                        # Populate domain name
+                        # print indicatorD.get('malware_family'),indicatorD.get('last_seen')
+                        indicator = Indicator(id_="indicator-{0}".format(indicatorD.get('uuid')), title=''.join(indicatorD.get('malware_family')), timestamp=indicatorD.get('last_seen'))
+                        indicator.add_indicator_type("Domain Watchlist")
+                        domain = DomainName()
+                        domain.value = indicatorD.get('key')
+                        domain.value.condition = "Equals"
+                        indicator.add_observable(domain)
+                        # print "Added domain"
 
-            indicator.add_observable(session)
-            # Link TTP
-            indicator.add_indicated_ttp(TTP(idref=ttps[i[3]].id_))
-            # Indicate confidence
-            indicator.confidence = Confidence(value=VocabString(i[8]))
-            # Add to package
-            stix_package.add_indicator(indicator)
+                    elif indicatorD.get('type') == "ip":
+                        # Create indicator
+                        indicator = Indicator(id_="indicator-{0}".format(indicatorD.get('uuid')), title=indicatorD.get('malware_family'), timestamp=indicatorD.get('last_seen'))
+                        indicator.add_indicator_type("IP Watchlist")
+                        # Populate IP address
+                        addr = Address(address_value=indicatorD.get('key'), category=Address.CAT_IPV4)
+                        addr.condition = "Equals"
+                        indicator.add_observable(addr)
+                        # print "Added IP"
 
-    # Output to XML
-    print stix_package.to_xml()
+                    # Link TTP
+                    indicator.add_indicated_ttp(TTP(idref=ttps[last_seen_as].id_))
+                    # Indicate confidence score
+                    indicator.confidence = Confidence(value=VocabString(indicatorD.get('confidence')))
+                    # Add related indicator to malware
+                    indicator.add_related_indicator(malware[md5])
+                    # Add to package
+                    stix_package.add_indicator(indicator)
+                    print "Added INDICATOR"
+
+            else:
+                print "API request couldn't be fulfilled due status code: %d" % r.status_code
+                more_data = False
+
+        # Output to XML
+        # print stix_package.to_xml()
+        file.write(stix_package.to_xml())
+        file.close()
+
+    except requests.exceptions.ConnectionError as e:
+        print "Check your network connection\n %s" % str(e)
+
+    except requests.exceptions.HTTPError as e:
+        print "Bad HTTP response\n %s" % str(e)
+
+    except Exception as e:
+        print "Uncaught exception\n %s" % str(e)
 
 if __name__ == '__main__':
     main()
